51817fb859c3aa96977009fbcdc43aae7b6b6367
L2IUASZ <l2iuasz@gmail.com>
2024-01-11 15:54:20 +0000

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Accueil from './Pages/Accueil';
+
+function App() {
+  return (
+    <div className="App">
+     <Accueil/>
+    </div>
+  );
+}
+
+export default App;
